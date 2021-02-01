2079de09ca1dd0df87006bbeacedbd91c39dee35
Hrithik Raj <iamhrithikraj26@gmail.com>
2021-02-02 03:25:51 +0530

    initial commit

---
+++ app/src/main/java/com/example/overlist/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.overlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int SPLASH = 5000;
+
+    Animation topAnimation, bottomAnimation;
+    ImageView logo;
+    TextView title, tagline;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        topAnimation = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        logo = findViewById(R.id.logo);
+        title = findViewById(R.id.title);
+        tagline = findViewById(R.id.tagline);
+
+        logo.setAnimation(topAnimation);
+        title.setAnimation(bottomAnimation);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+            startActivity(intent);
+            finish();
+        },SPLASH);
+    }
+}
